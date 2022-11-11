432c59d825d705446f26750992d00e21b284ac8a
Mihai Casu <mihai.casu@keysight.com>
2022-11-11 16:28:52 +0200

    compomemts split

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddItemComponent } from './add-item/add-item.component';
+import { HeaderComponent } from './header/header.component';
+import { ListItemsComponent } from './list-items/list-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddItemComponent,
+    HeaderComponent,
+    ListItemsComponent
   ],
   imports: [
     BrowserModule
