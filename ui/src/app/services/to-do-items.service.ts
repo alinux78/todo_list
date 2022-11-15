c77a0619e734b671f96be447e1b62341c3e29183
Mihai Casu <mihai.casu@keysight.com>
2022-11-15 15:33:30 +0200

    added createdAt

--- ui/src/app/services/to-do-items.service.ts
+++ ui/src/app/services/to-do-items.service.ts
@@ -7,25 +7,30 @@ export interface ToDoItem {
 	summary: string;
 	details?: number;
 	dueDate?: Date;
+  createdAt: Date;
 }
 
 let items: Array<ToDoItem> = [
   {
     done: true,
     summary: "Buy groceries for next week",
+    createdAt: new Date()
   },
   {
     done: false,
     summary: "Renew car insurance",
-    dueDate: new Date((new Date().getTime() + 7 * 24 * 60 * 60 * 1000))
+    dueDate: new Date((new Date().getTime() + 7 * 24 * 60 * 60 * 1000)),
+    createdAt: new Date()
   },
   {
     done: false,
-    summary: "Sign up for online course"
+    summary: "Sign up for online course",
+    createdAt: new Date()
   },
   {
     done: false,
-    summary: "Buy Christmas presents"
+    summary: "Buy Christmas presents",
+    createdAt: new Date()
   }
 
 
