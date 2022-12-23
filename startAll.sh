5c869656dde34bc3f64b051f930bb3dc308233ba
Mihai Casu <mihai.casu@gmail.com>
2022-12-23 16:07:30 +0200

    startAll now stops all services before exiting

--- startAll.sh
+++ startAll.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+clean_up () {
+    ARG=$?
+    docker stop postgresql
+    docker stop pgadmin
+    kill $SPRING_PID
+    echo "done"
+    exit $ARG
+} 
+trap clean_up EXIT
+
+
 docker start postgresql
 docker start pgadmin
 
 cd ./spring-backend/
 ./start.sh &
+SPRING_PID=$?
 cd -
 
 
