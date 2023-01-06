0c90f6bb67051f8e5f742ddab360380e291f8e96
Mihai Casu <mihai.casu@keysight.com>
2023-01-06 18:41:17 +0200

    externalized db config to env vars

--- spring-backend/start.sh
+++ spring-backend/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+export POSTGRES_HOST=${POSTGRES_HOST:-localhost}
+export POSTGRES_PORT=${POSTGRES_PORT:-5432}
+export POSTGRES_USER=${POSTGRES_USER:-todo}
+export POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-todo}
+
 ./mvnw spring-boot:run
