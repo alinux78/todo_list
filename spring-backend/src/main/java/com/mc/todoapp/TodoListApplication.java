ba75da7b9b190431f26ab9f660f89c2eec84cc3e
Mihai Casu <mihai.casu@keysight.com>
2023-01-13 12:43:28 +0200

    extracted spring config

--- spring-backend/src/main/java/com/mc/todoapp/TodoListApplication.java
+++ spring-backend/src/main/java/com/mc/todoapp/TodoListApplication.java
@@ -1,5 +1,6 @@
 package com.mc.todoapp;
 
+import java.time.Year;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
@@ -8,8 +9,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.mc.todoapp.models.TodoItem;
 import com.mc.todoapp.repositories.TodoItemsRepository;
@@ -22,17 +21,7 @@ public class TodoListApplication {
 		SpringApplication.run(TodoListApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("http://localhost:5050");
-			}
-		};
-	}
 
-	// TODO remove init data
 	@Bean
 	ApplicationRunner init(TodoItemsRepository repository) {
 		return args -> {
@@ -46,8 +35,8 @@ public class TodoListApplication {
 		long now = System.currentTimeMillis();
 		int i = 0;
 		var items =List.of(
-            new TodoItem(null, true, "Write letter to Santa",  new GregorianCalendar(2012, Calendar.NOVEMBER, 28).getTimeInMillis(), now + (i++) * 1000),
-            new TodoItem(null, false, "Buy presents (just in case)",  new GregorianCalendar(2022, Calendar.DECEMBER, 23).getTimeInMillis(), now + (i++) * 1000),
+            new TodoItem(null, true, "Write letter to Santa",  new GregorianCalendar(Year.now().getValue(), Calendar.NOVEMBER, 28).getTimeInMillis(), now + (i++) * 1000),
+            new TodoItem(null, false, "Buy presents (just in case)",  new GregorianCalendar(Year.now().getValue(), Calendar.DECEMBER, 23).getTimeInMillis(), now + (i++) * 1000),
             new TodoItem(null, false, "Renew car insurance",  new GregorianCalendar(2023, Calendar.JANUARY, 15).getTimeInMillis(), now + (i++) * 1000),
             new TodoItem(null, false, "Read Harry Potter",  null, now + (i++) * 1000),
             new TodoItem(null, false, "Order coffee",  null, now + (i++) * 1000)
