3fece804fb09be31b04a03bd73cd7ce928351662
Mihai Casu <mihai.casu@keysight.com>
2023-05-22 10:51:17 +0300

    test spring event listener

--- spring-backend/src/main/java/com/mc/todoapp/TodoListApplication.java
+++ spring-backend/src/main/java/com/mc/todoapp/TodoListApplication.java
@@ -5,18 +5,24 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 
 import com.mc.todoapp.models.TodoItem;
 import com.mc.todoapp.repositories.TodoItemsRepository;
 
 
+
 @SpringBootApplication
 public class TodoListApplication {
-
+	
+	Logger logger = LoggerFactory.getLogger(TodoListApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
 	}
@@ -29,6 +35,12 @@ public class TodoListApplication {
 				addDemoData(repository);
 			}
 		};
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void initData() {
+		// TODO move init data here?
+		logger.info("init event listener");
 	}
 
 	private void addDemoData(TodoItemsRepository repository) {
