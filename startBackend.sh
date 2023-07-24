290d5d6fcd2694d5ffb0ee6ada370361bdb1abf6
Mihai Casu <mihai.casu@keysight.com>
2023-07-24 18:23:07 +0300

    scrip for starting backend

---
+++ startBackend.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clean_up () {
+    ARG=$?
+    docker stop postgresql
+    docker stop pgadmin
+    docker stop keycloak
+    curl -X POST http://localhost:8082/actuator/shutdown
+    echo "done"
+    exit $ARG
+} 
+trap clean_up EXIT
+
+
+docker start postgresql
+docker start pgadmin
+docker start keycloak
+
+cd ./spring-backend/
+./start.sh
+
+
